df9696e383a0aa2851edfae4444745517f590116
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2023-09-20 21:55:44 -0300

    Waves added to Welcome section, responsivness implemented

--- src/app/components/welcome.tsx
+++ src/app/components/welcome.tsx
@@ -2,12 +2,23 @@ import React from 'react'
 import styles from '../styles/page.module.css'
 import SouthEastIcon from '@mui/icons-material/SouthEast';
 import Button from '@mui/material-next/Button';
+import Image from 'next/image'
 import { WELCOME_OUR_STORY, WELCOME_SUBTITLE, WELCOME_SUBTITLE_TEXT, WELCOME_TITLE } from '../texts/homeTexts';
 
 function Welcome() {
   return (
     <>
         <section className={styles.welcome}>
+            <div className={styles.wave}>
+                <Image 
+                    src='../wave-group.svg' 
+                    alt="waves" 
+                    layout="fill" 
+                    // width={1111} height={1000} 
+                    objectFit="cover" 
+                />
+                {/* <Image src='../wave-group.svg' alt="waves" layout="fill" objectFit="cover" /> */}
+            </div>
             <div className={`${styles['welcome-title']}`}>
                 <h1>{WELCOME_TITLE}</h1>
             </div>
